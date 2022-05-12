b9b945666f3a6a1be1b1a4d29f5d93c90b5c109c
Dmitry <dmitri.arovelti@gmail.com>
2022-05-12 23:51:26 +0300

    First try

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,12 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	n := len(input)
+	reverse := make([]rune, n)
+	for _, l := range input {
+		n--
+		reverse[n] = l
+	}
+	output = string(reverse)
 	return output
 }
